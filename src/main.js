e92f7c297823cc4079fdbb322da64136d31d3031
Abhi10699 <patelabhi10699@gmail.com>
2018-09-24 23:17:15 +0530

    Electron Main File

---
+++ src/main.js
@@ -0,0 +1,29 @@
+const {app, BrowserWindow} = require('electron')
+let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 1600, height: 720})
+  mainWindow.setMenu(null)
+  // and load the index.html of the app.
+  mainWindow.loadURL('http://localhost:3000')
+
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
