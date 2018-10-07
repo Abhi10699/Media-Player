9941032f215e350388067d78878f285275249551
Abhi10699 <patelabhi10699@gmail.com>
2018-10-07 23:21:42 +0530

    Added List Component and removed videos component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import ReactDOM from 'react-dom';
 // Components
 import Videos from './Components/Videos/Videos';
 import Navbar from './Components/Navbar/Navbar';
+import List from './Components/List/List';
+
 // Css
 import './App.css'
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<div>
-    <Videos class="main"/>
-</div>,document.getElementById('root'));
+ReactDOM.render(
+<div className="mainContainer">
+    <Navbar />
+    <List/>
+</div>
+,document.getElementById('root'));
 registerServiceWorker();
