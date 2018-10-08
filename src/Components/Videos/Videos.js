51413f850ee96187d85529340c15467e6d292586
Abhi10699 <patelabhi10699@gmail.com>
2018-10-09 01:20:25 +0530

    Now Loads ID's over props

--- src/Components/Videos/Videos.js
+++ src/Components/Videos/Videos.js
@@ -9,7 +9,7 @@ class Videos extends Component{
             
             <div class="iframe_container">
                 <div className="iframe">
-                <iframe width="100%" height="100%" src={"https://www.youtube.com/embed/"+this.props.videoId+"?rel=0&autoplay=1"} frameBorder="0"allowFullscreen="true" id="Iframe"></iframe>
+                <iframe width="100%" height="100%" src={"https://www.youtube.com/embed/"+this.props.id+"?rel=0&autoplay=1"} frameBorder="0"allowFullscreen="true" id="Iframe"></iframe>
                 </div>
             </div>  
             
