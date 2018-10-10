a445612d48c66eec19eef95b5340d63545c71c1d
Abhi10699 <patelabhi10699@gmail.com>
2018-10-10 22:04:02 +0530

    Removed Iframe and added react-youtube. Fixes #4

--- src/Components/Videos/Videos.js
+++ src/Components/Videos/Videos.js
@@ -1,18 +1,27 @@
 import React,{Component} from 'react'
 import './videos.css'
+import YouTube from 'react-youtube';
 class Videos extends Component{
+     
     constructor(props) {
         super(props);
+        this.opts = {
+          playerVars:{
+            autoplay:1,
+            rel:0
+          }
+        }
     }
+
+    _onEnd(e){
+      console.log("Video Ended.");
+    }
+
     render(){
-        return(
-            
-            <div class="iframe_container">
-                <div className="iframe">
-                <iframe width="100%" height="100%" src={"https://www.youtube.com/embed/"+this.props.id+"?rel=0&autoplay=1"} frameBorder="0"allowFullscreen="true" id="Iframe"></iframe>
-                </div>
-            </div>  
-            
+        return(    
+          <div class="iframe_container">  
+            <YouTube videoId={this.props.id} className="iframe" opts={this.opts} onEnd={this._onEnd}></YouTube>
+          </div>    
         )
     }
 }
