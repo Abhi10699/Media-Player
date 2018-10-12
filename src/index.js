be342834efc6a213b686ef84d16df747127986c6
Abhi10699 <patelabhi10699@gmail.com>
2018-10-13 00:02:08 +0530

    Added: Routing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route,Link} from 'react-router-dom';
 
 // Components
 import Parent from './Components/Parent/Parent';
@@ -10,8 +11,10 @@ import './App.css'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
+<Router>
 <div>
     <Parent/>
 </div>
+</Router>
 ,document.getElementById('root'));
 registerServiceWorker();
