933a5427826dd2f0e11d2054b3f266b69ac89847
Abhi10699 <patelabhi10699@gmail.com>
2018-10-12 23:59:33 +0530

    Added: Quick-Access Component

---
+++ src/Components/QuickAccess/QuickAccess.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+
+export default class QuickAccess extends Component{
+  render(){
+    return(
+      <h1 style={{textAlign:"center"}}>Quick Access</h1>
+    )
+  }
+}
