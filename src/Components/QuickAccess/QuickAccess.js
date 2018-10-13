53598c8a043075bb6903242f16c49b9f9aabdb05
Abhi10699 <patelabhi10699@gmail.com>
2018-10-13 21:57:26 +0530

    Fixes #8

--- src/Components/QuickAccess/QuickAccess.js
+++ src/Components/QuickAccess/QuickAccess.js
@@ -2,6 +2,56 @@ import React,{Component} from 'react';
 import Modal from './playlistModal';
 
 export default class QuickAccess extends Component{
+  
+  constructor(){
+    super();
+    this.state = {
+      PlaylistItems:[],
+      isEmpty:true
+    }
+  }
+
+  componentWillMount(){
+   this.checkStorage()
+  }
+
+  checkStorage = ()=>{
+    let storageData = JSON.parse(localStorage.getItem("PlayListData"))
+    if(storageData !== null){
+      this.setState({isEmpty:false})
+      this.setState({PlaylistItems:storageData})
+    }
+  }
+
+  changePlaylist = (e)=>{
+    this.props.loadPlaylist(e.target.attributes.getNamedItem('playlistId').value);
+  }
+
+  checkRender(){
+    if(this.state.isEmpty){
+      return(
+        <div>
+          <h3>Add Some Playlists to fill this up!</h3>
+        </div>
+      )
+    }else{
+      return(
+        this.state.PlaylistItems.map(data=>{
+          return(
+            <li className="list-group-item mt-2">
+            {data.alias} 
+            <button 
+            className="btn btn-primary btn-sm" 
+            style={{float:"right"}} 
+            playlistId = {data.playlistId}
+            onClick = {this.changePlaylist.bind(this)}>Play</button>
+            </li>
+          )
+        })
+      )
+    }
+  }
+  
   render(){
     return(
       <div>
@@ -9,15 +59,13 @@ export default class QuickAccess extends Component{
           <a class="navbar-brand" href="#">Your Playlists</a>
           <button className="btn" style={{backgroundColor:"black"}} data-toggle="modal" data-target="#exampleModal"><span className="fa fa-plus fa-lg mt-2" style={{color:"white"}}></span></button>
         </nav>
-        <Modal id="exampleModal"/>
+        <Modal id="exampleModal" newPlaylistLoaded = {this.checkStorage.bind(this)}/>
 
         <div className="container mt-3">
         <ul class="list-group">
-          <li class="list-group-item">Cras justo odio</li>
-          <li class="list-group-item">Dapibus ac facilisis in</li>
-          <li class="list-group-item">Morbi leo risus</li>
-          <li class="list-group-item">Porta ac consectetur ac</li>
-          <li class="list-group-item">Vestibulum at eros</li>
+            {
+             this.checkRender()
+            }
         </ul>
         </div>
       </div>
