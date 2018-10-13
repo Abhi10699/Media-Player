96f911589abddcabaa8443ce098baf4f6ec8fb9d
Abhi10699 <patelabhi10699@gmail.com>
2018-10-13 18:27:34 +0530

    Added: Imports for Bootstrap, Jquery and Popper

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route,Link} from 'react-router-dom';
 
+
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+// Popper
+import 'popper.js'
+
 // Components
 import Parent from './Components/Parent/Parent';
 
