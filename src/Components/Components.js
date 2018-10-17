7f987f107e1b89e4b8f1a86726deb76b0e8299d5
Abhi10699 <patelabhi10699@gmail.com>
2018-10-17 23:38:44 +0530

    Imported: Settings and Loader Component

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -2,5 +2,7 @@ import List from './List/List';
 import Navbar from './Navbar/Navbar';
 import Videos from './Videos/Videos'
 import QuickAccess from './QuickAccess/QuickAccess';
+import Settings from './Settings/Settings'
+import Loader from './Parent/Loader'
 
-export {List,Navbar,Videos,QuickAccess};
+export {List,Navbar,Videos,QuickAccess,Settings,Loader};
