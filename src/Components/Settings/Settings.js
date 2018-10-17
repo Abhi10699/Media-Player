a49f35c5b8851dab52e2df849dbed3cc9619a7c8
Abhi10699 <patelabhi10699@gmail.com>
2018-10-17 23:37:43 +0530

    Added: Settings Page

---
+++ src/Components/Settings/Settings.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+class Settings extends Component{
+  render(){
+    return(
+      <div>
+         <nav class="navbar navbar-dark bg-dark">
+          <a class="navbar-brand" href="#">Settings</a>
+        </nav>
+      </div>
+    )
+  }
+};
+export default Settings;
