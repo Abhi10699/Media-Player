c501ff18e1d8c5247bf63df5c8417d44f233e05d
Abhi10699 <patelabhi10699@gmail.com>
2018-10-17 23:37:12 +0530

    Added Toggle Menu

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,45 +1,70 @@
 import React, {Component, Fragment} from 'react';
+import NavbarToggle from './NavbarToggler' 
+
 import './list.css';
+
 export default class List extends Component{  
-    constructor(){
+  constructor(){
     super();
     this.state ={
-        id:""
+      id:""
     }
   }
 
   updateId(e){
-    this.props.updateIndex(e.target.attributes.getNamedItem('vidIndex').value);
+    let index = this.props.updateIndex;
+    index(e.target.attributes.getNamedItem('vidIndex').value);
     e.preventDefaults;
+  }
+
+  componentDidMount(){
+    this.setState({id:this.props.curSong})
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log(props,state)
+    if(props.curSong !== state.id){
+      return{id:props.curSong}
+    }
+  }
+
+  renderList(){
+    return(
+      <ul class="list-group">
+        {
+          this.props.videos.map((item,index)=>{
+            return(
+              <li className="list-group-item">
+                <a className="link" 
+                  vidid={item.snippet.resourceId.videoId} 
+                  vidIndex={index} 
+                  onClick={this.updateId.bind(this)}>
+                  {(index + 1)+")" + " " + item.snippet.title}
+                </a>
+              </li>
+            )
+          }
+        )
+      }
+    </ul>)
   }
   render(){
     return(
       <Fragment>
           <nav class="navbar navbar-dark bg-dark">
-          <a class="navbar-brand" href="#">Songs</a>
+            <a class="navbar-brand" href="#">Videos</a>
+            <button class="navbar-toggler" 
+              type="button" data-toggle="collapse" 
+              data-target="#navbarTogglerDemo01">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <NavbarToggle id="navbarTogglerDemo01" curSong={this.state.id} Videos = {this.props.videos}/>
         </nav>
         <div className="list_container">
-            <div className="list_child list">
-                <ul class="list-group">
-                    {
-                        this.props.videos.map((item,index)=>{
-                            return(
-                                <li className="list-group-item">
-                                    <a
-                                    className="link"
-                                    vidid={item.snippet.resourceId.videoId}
-                                    vidIndex={index}
-                                    onClick={this.updateId.bind(this)}
-                                    >{(index + 1)+")" + " " + item.snippet.title}
-                                    </a>
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-            </div>
+          <div className="list_child list">
+            {this.renderList()}
+          </div>
         </div>
     </Fragment>
-    )
-}
+  )}
 }
